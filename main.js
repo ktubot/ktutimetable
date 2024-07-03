9149558a56df31fd284935942a89d1f3aad8e193
ktubot <121492934+ktubot@users.noreply.github.com>
2024-07-03 21:27:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,12 @@
 const tableBody = document.getElementById('table-body');
 const examsDetails = {
-    "S1": {
-        "CS": [
-            { "code": "MAT101", "date": "May 26, 2024 9:30:00", "name": "Linear Algebra And Calculus", "id": 1 },
-            {"code": "PHT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Physics", "id":2},
-            {"code": "EST100", "date": "June 5, 2024 9:30:00", "name": "Engineering Mechanics", "id":3},
-            {"code": "EST120", "date": "June 10, 2024 9:30:00", "name": "BME & BCE", "id":4},
-            {"code": "HUN102", "date": "July 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
-            {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
-        ],
-        "EC":[
-            { "code": "MAT102", "date": "May 25, 2024 9:30:00", "name": "Vector Calculus, and Differential Equations and Transforms", "id": 1 },
-            {"code": "PHT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Physics", "id":2},
-            {"code": "EST110", "date": "June 5, 2024 9:30:00", "name": "Engineering Graphics", "id":3},
-            {"code": "EST130", "date": "June 10, 2024 9:30:00", "name": "ECE & EEE", "id":4},
-            {"code": "HUN102", "date": "June 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
-            {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
-
-        ],
-        "ME":[
-            { "code": "MAT102", "date": "May 25, 2024 9:30:00", "name": "Vector Calculus, and Differential Equations and Transforms", "id": 1 },
-            {"code": "PHT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Physics", "id":2},
-            {"code": "EST110", "date": "June 5, 2024 9:30:00", "name": "Engineering Graphics", "id":3},
-            {"code": "EST130", "date": "June 10, 2024 9:30:00", "name": "ECE & EEE", "id":4},
-            {"code": "HUN102", "date": "June 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
-            {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
-
-        ],
-        "EE":[
-            { "code": "MAT102", "date": "May 25, 2024 9:30:00", "name": "Vector Calculus, and Differential Equations and Transforms", "id": 1 },
-            {"code": "PHT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Physics", "id":2},
-            {"code": "EST110", "date": "June 5, 2024 9:30:00", "name": "Engineering Graphics", "id":3},
-            {"code": "EST130", "date": "June 10, 2024 9:30:00", "name": "ECE & EEE", "id":4},
-            {"code": "HUN102", "date": "June 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
-            {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
-
-        ],
-    },
     "S2": {
         "CS": [
             { "code": "MAT102", "date": "May 25, 2024 9:30:00", "name": "Vector Calculus, and Differential Equations and Transforms", "id": 1 },
             {"code": "CYT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Chemistry", "id":2},
             {"code": "EST100", "date": "June 5, 2024 9:30:00", "name": "Engineering Mechanics", "id":3},
             {"code": "EST120", "date": "June 10, 2024 9:30:00", "name": "BME & BCE", "id":4},
-            {"code": "HUN102", "date": "July 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
+            {"code": "HUN102", "date": "June 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
             {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
         ],
         "EC":[
@@ -63,49 +26,6 @@ const examsDetails = {
             {"code": "HUN102", "date": "June 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
             {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
 
-        ],
-
-    },
-    "S3": {
-        "CS": [
-            { "code": "MAT203", "date": "May 25, 2024 9:30:00", "name": "Discrete Mathematical Structures", "id": 1 },
-            {"code": "CST201", "date": "May 31, 2024 9:30:00", "name": "Data Structures", "id":2},
-            {"code": "CST203", "date": "June 5, 2024 9:30:00", "name": "Engineering Mechanics", "id":3},
-            {"code": "CST205", "date": "June 10, 2024 9:30:00", "name": "BME & BCE", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "EC": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "ECT201", "date": "May 31, 2024 9:30:00", "name": "Solid State Devices", "id":2},
-            {"code": "ECT203", "date": "June 5, 2024 9:30:00", "name": "Logic Circuit Design", "id":3},
-            {"code": "ECT205", "date": "June 10, 2024 9:30:00", "name": "Network Theory", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "ME": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "MET201", "date": "May 31, 2024 9:30:00", "name": "Mechanics Of Solids", "id":2},
-            {"code": "MET203", "date": "June 5, 2024 9:30:00", "name": "Mechanics Of Fluids", "id":3},
-            {"code": "MET205", "date": "June 10, 2024 9:30:00", "name": "Metallurgy & Material Science", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "CE": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "CET201", "date": "May 31, 2024 9:30:00", "name": "Mechanics Of Solids", "id":2},
-            {"code": "CET203", "date": "June 5, 2024 9:30:00", "name": "Fluid Mechanics & Hydraulics", "id":3},
-            {"code": "CET205", "date": "June 10, 2024 9:30:00", "name": "Surveying & Geomatics", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "EE": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "EET201", "date": "May 31, 2024 9:30:00", "name": "Circuits And Networks", "id":2},
-            {"code": "EET203", "date": "June 5, 2024 9:30:00", "name": "Measurements & Instrumentation ", "id":3},
-            {"code": "EET205", "date": "June 10, 2024 9:30:00", "name": "Analog Electronics", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
         ],
 
     },
@@ -136,49 +56,6 @@ const examsDetails = {
             {"code": "MET206", "date": "June 7, 2024 9:30:00", "name": "FLUID MACHINERY", "id":4},
             {"code": "HUT200", "date": "June 12, 2024 9:30:00", "name": "PROFESSIONAL ETHICS", "id":5},
             {"code": "MCN202", "date": "June 15, 2024 9:30:00", "name": "CONSTITUTION OF INDIA", "id":6},
-        ],
-
-    },
-    "S5": {
-        "CS": [
-            { "code": "CST301", "date": "August 25, 2024 09:30:00", "name": "Formal Languages & Automata Theory", "id": 1 },
-            {"code": "CST303", "date": "May 31, 2024 9:30:00", "name": "Data Structures", "id":2},
-            {"code": "CST305", "date": "June 5, 2024 9:30:00", "name": "Engineering Mechanics", "id":3},
-            {"code": "CST307", "date": "June 10, 2024 9:30:00", "name": "BME & BCE", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "EC": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "ECT201", "date": "May 31, 2024 9:30:00", "name": "Solid State Devices", "id":2},
-            {"code": "ECT203", "date": "June 5, 2024 9:30:00", "name": "Logic Circuit Design", "id":3},
-            {"code": "ECT205", "date": "June 10, 2024 9:30:00", "name": "Network Theory", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "ME": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "MET201", "date": "May 31, 2024 9:30:00", "name": "Mechanics Of Solids", "id":2},
-            {"code": "MET203", "date": "June 5, 2024 9:30:00", "name": "Mechanics Of Fluids", "id":3},
-            {"code": "MET205", "date": "June 10, 2024 9:30:00", "name": "Metallurgy & Material Science", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "CE": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "CET201", "date": "May 31, 2024 9:30:00", "name": "Mechanics Of Solids", "id":2},
-            {"code": "CET203", "date": "June 5, 2024 9:30:00", "name": "Fluid Mechanics & Hydraulics", "id":3},
-            {"code": "CET205", "date": "June 10, 2024 9:30:00", "name": "Surveying & Geomatics", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
-        ],
-        "EE": [
-            { "code": "MAT201", "date": "May 25, 2024 9:30:00", "name": "Partial Differential Equation & Complex Analysis", "id": 1 },
-            {"code": "EET201", "date": "May 31, 2024 9:30:00", "name": "Circuits And Networks", "id":2},
-            {"code": "EET203", "date": "June 5, 2024 9:30:00", "name": "Measurements & Instrumentation ", "id":3},
-            {"code": "EET205", "date": "June 10, 2024 9:30:00", "name": "Analog Electronics", "id":4},
-            {"code": "HUT200/EST200", "date": "July 13, 2024 9:30:00", "name": "Design And Engineering / Professional Communication", "id":5},
-            {"code": "MCN201", "date": "June 18, 2024 9:30:00", "name": "Sustainable Engineering", "id":6},
         ],
 
     },
@@ -239,45 +116,6 @@ const examsDetails = {
         ],
         
     },
-    "S7": {
-        "CS": [
-            { "code": "CST401", "date": "May 22, 2024 9:30:00", "name": "Artificial Intelligence", "id": 1 },
-            {"code": "CST4XX", "date": "May 27, 2024 9:30:00", "name": "Elective - II", "id":2},
-            {"code": "CST4XX", "date": "May 30, 2024 9:30:00", "name": "Elective - Open", "id":3},
-            {"code": "MCN401", "date": "June 3, 2024 9:30:00", "name": "Industrial Safety Engineering", "id":4},
-        ],
-        "EC":[
-            { "code": "ECT401", "date": "May 22, 2024 9:30:00", "name": "Wireless Communication", "id": 1 },
-            {"code": "ECT4XX", "date": "May 27, 2024 9:30:00", "name": "Elective - II", "id":2},
-            {"code": "ECT4XX", "date": "May 30, 2024 9:30:00", "name": "Elective - Open", "id":3},
-            {"code": "MCN401", "date": "June 3, 2024 9:30:00", "name": "Industrial Safety Engineering", "id":4},
-        ],
-        "EE":[
-            { "code": "EET401", "date": "May 22, 2024 9:30:00", "name": "Advanced Control Systems", "id": 1 },
-            {"code": "EET4XX", "date": "May 27, 2024 9:30:00", "name": "Elective - II", "id":2},
-            {"code": "EET4XX", "date": "May 30, 2024 9:30:00", "name": "Elective - Open", "id":3},
-            {"code": "MCN401", "date": "June 3, 2024 9:30:00", "name": "Industrial Safety Engineering", "id":4},
-        ],
-        "IT":[
-            { "code": "ITT401", "date": "May 22, 2024 9:30:00", "name": "Data Analytics ", "id": 1 },
-            {"code": "ITT4XX", "date": "May 27, 2024 9:30:00", "name": "Elective - II", "id":2},
-            {"code": "ITT4XX", "date": "May 30, 2024 9:30:00", "name": "Elective - Open", "id":3},
-            {"code": "MCN401", "date": "June 3, 2024 9:30:00", "name": "Industrial Safety Engineering", "id":4},
-        ],
-        "ME":[
-            { "code": "MET401", "date": "May 22, 2024 9:30:00", "name": "Design Of Machine Elements", "id": 1 },
-            {"code": "MET4XX", "date": "May 27, 2024 9:30:00", "name": "Elective - II", "id":2},
-            {"code": "MET4XX", "date": "May 30, 2024 9:30:00", "name": "Elective - Open", "id":3},
-            {"code": "MCN401", "date": "June 3, 2024 9:30:00", "name": "Industrial Safety Engineering", "id":4},
-        ],
-        "CE":[
-            { "code": "CET401", "date": "May 22, 2024 9:30:00", "name": "Design Of Steel Structures ", "id": 1 },
-            {"code": "CET4XX", "date": "May 27, 2024 9:30:00", "name": "Elective - II", "id":2},
-            {"code": "CET4XX", "date": "May 30, 2024 9:30:00", "name": "Elective - Open", "id":3},
-            {"code": "MCN401", "date": "June 3, 2024 9:30:00", "name": "Industrial Safety Engineering", "id":4},
-        ],
-    }
-
     "S8": {
         "CS": [
             { "code": "CST402", "date": "May 22, 2024 9:30:00", "name": "Distributed Computing", "id": 1 },
