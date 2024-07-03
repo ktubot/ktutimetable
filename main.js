b41b530bdfbdf6c00df66f891af85719c6473ed4
ktubot <121492934+ktubot@users.noreply.github.com>
2024-07-03 14:37:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -66,6 +66,35 @@ const examsDetails = {
         ],
 
     },
+    "S3": {
+        "CS": [
+            { "code": "MAT102", "date": "May 25, 2024 9:30:00", "name": "Vector Calculus, and Differential Equations and Transforms", "id": 1 },
+            {"code": "CYT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Chemistry", "id":2},
+            {"code": "EST100", "date": "June 5, 2024 9:30:00", "name": "Engineering Mechanics", "id":3},
+            {"code": "EST120", "date": "June 10, 2024 9:30:00", "name": "BME & BCE", "id":4},
+            {"code": "HUN102", "date": "July 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
+            {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
+        ],
+        "EC":[
+            { "code": "MAT102", "date": "May 25, 2024 9:30:00", "name": "Vector Calculus, and Differential Equations and Transforms", "id": 1 },
+            {"code": "PHT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Physics", "id":2},
+            {"code": "EST110", "date": "June 5, 2024 9:30:00", "name": "Engineering Graphics", "id":3},
+            {"code": "EST130", "date": "June 10, 2024 9:30:00", "name": "ECE & EEE", "id":4},
+            {"code": "HUN102", "date": "June 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
+            {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
+
+        ],
+        "ME":[
+            { "code": "MAT102", "date": "May 25, 2024 9:30:00", "name": "Vector Calculus, and Differential Equations and Transforms", "id": 1 },
+            {"code": "PHT100", "date": "May 31, 2024 9:30:00", "name": "Engineering Physics", "id":2},
+            {"code": "EST110", "date": "June 5, 2024 9:30:00", "name": "Engineering Graphics", "id":3},
+            {"code": "EST130", "date": "June 10, 2024 9:30:00", "name": "ECE & EEE", "id":4},
+            {"code": "HUN102", "date": "June 13, 2024 9:30:00", "name": "Professional Communication", "id":5},
+            {"code": "EST102", "date": "June 18, 2024 9:30:00", "name": "Programming in C", "id":6},
+
+        ],
+
+    },
     "S4": {
         "CS": [
             {"code": "MAT206", "date": "May 24, 2024 9:30:00", "name": "GRAPH THEORY", "id": 1 },
